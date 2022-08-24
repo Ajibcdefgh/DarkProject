bdc50540e44e125f679924c893221cf980e8c347
DarkoBunny <darkobunny57@gmail.com>
2022-08-25 00:24:02 +0700

    hehe

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -112,16 +112,12 @@ async def set_var(var):
 """
 
 
-@register(outgoing=True, pattern=r"^\.usage$")
+@register(outgoing=True, pattern=r"^\.usage(?: |$)")
 async def dyno_usage(dyno):
     """
     Get your account Dyno Usage
     """
-    if app is None:
-        return await dyno.edit(
-            "**Please setup your** `HEROKU_APP_NAME` **and** `HEROKU_API_KEY`**.**"
-        )
-    await dyno.edit("**Processing...**")
+    await dyno.edit("`Mendapatkan informasi...`")
     user_id = Heroku.account().id
     path = "/accounts/" + user_id + "/actions/get-quota"
     async with aiohttp.ClientSession() as session:
@@ -140,18 +136,18 @@ async def dyno_usage(dyno):
                 await dyno.client.send_message(
                     dyno.chat_id, f"`{r.reason}`", reply_to=dyno.id
                 )
-                await dyno.edit("**Error: Heroku is being Heroku.**")
+                await dyno.edit("`Tidak bisa mendapatkan informasi...`")
                 return False
             result = await r.json()
             quota = result["account_quota"]
             quota_used = result["quota_used"]
-            """ - User Quota Limit and Used - """
+
             remaining_quota = quota - quota_used
             percentage = math.floor(remaining_quota / quota * 100)
             minutes_remaining = remaining_quota / 60
             hours = math.floor(minutes_remaining / 60)
             minutes = math.floor(minutes_remaining % 60)
-            """ - User App Used Quota - """
+
             Apps = result["apps"]
             for apps in Apps:
                 if apps.get("app_uuid") == app.id:
@@ -166,9 +162,14 @@ async def dyno_usage(dyno):
             AppMinutes = math.floor(AppQuotaUsed % 60)
 
             await dyno.edit(
-                "**Heroku dyno hour stats for current month**\n\n"
-                f"**Usage ({app.name}):** {AppHours} hour(s), {AppMinutes} minute(s) - {AppPercentage}%\n"
-                f"**Remaining (total):** {hours} hour(s), {minutes} minute(s) - {percentage}%"
+                "**Penggunaan Dyno**:\n\n"
+                f"-> `Penggunaan Dyno untuk`  **{app.name}**:\n"
+                f"     •  **{AppHours} jam, "
+                f"{AppMinutes} menit  -  {AppPercentage}%**"
+                "\n\n"
+                "-> `Sisa Dyno di bulan ini`:\n"
+                f"     •  **{hours} jam, {minutes} menit  "
+                f"-  {percentage}%**"
             )
             return True
 
