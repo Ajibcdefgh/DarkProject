21a90072f6a53161d84aa2b42419c5baba64eede
pocong <81405011+poocong@users.noreply.github.com>
2022-09-23 21:00:50 +0700

    [ + ] add new module

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -89,4 +89,41 @@ async def _(event):
     os.remove(file)
     os.remove(ik)
 
+@register(outgoing=True, pattern=r"^\.frog (.*)")
+async def honkasays(event):
+    await event.edit("`Sedang Memprosess!!!`")
+    text = event.pattern_match.group(1)
+    if not text:
+        return await event.edit("`Beri Aku Bebeberapa Teks, Contoh .frog test`")
+    try:
+        if not text.endswith("."):
+            text = text + "."
+        if len(text) <= 9:
+            results = await bot.inline_query("honka_says_bot", text)
+            await results[2].click(
+                event.chat_id,
+                silent=True,
+                hide_via=True,
+            )
+        elif len(text) >= 14:
+            results = await bot.inline_query("honka_says_bot", text)
+            await results[0].click(
+                event.chat_id,
+                silent=True,
+                hide_via=True,
+            )
+        else:
+            results = await bot.inline_query("honka_says_bot", text)
+            await results[1].click(
+                event.chat_id,
+                silent=True,
+                hide_via=True,
+            )
+        await event.delete()
+    except ChatSendInlineForbiddenError:
+        await event.edit("`Maaf Saya tidak bisa menggunakan hal-hal sebaris di sini...`")
+    except ChatSendStickersForbiddenError:
+        await event.edit("Maaf saya tidak bisa mengirim stiker ke sini !!")
+
+
 CMD_HELP.update({"tiny": ">`.tiny [Reply to media or sticker`\n" "Usage: reduce media or sticker size\n\n"})
