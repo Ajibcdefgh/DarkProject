4f9b94e5d0377d8ec68146111afe96c43039e50f
pocong <81405011+poocong@users.noreply.github.com>
2022-09-23 20:33:41 +0700

    -

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -109,14 +109,3 @@ def async_wrap(func):
         return await loop.run_in_executor(executor, pfunc)
 
     return run
-
-async def bash(cmd):
-    process = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    err = stderr.decode().strip()
-    out = stdout.decode().strip()
-    return out, err
